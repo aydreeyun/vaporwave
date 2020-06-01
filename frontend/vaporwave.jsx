264462e9429f12eefa9d4310692688f5a004fbea
aydreeyun <uncoadrian@gmail.com>
2020-05-31 23:38:39 -0400

    created store/added tests to entry

--- frontend/vaporwave.jsx
+++ frontend/vaporwave.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
 
   ReactDOM.render(<h1>VaporWave</h1>, root);
+
+  // TESTS
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
 });
