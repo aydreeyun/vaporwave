f802e2f41f46c9f4a31933a3e0b3552eaa0034dc
aydreeyun <uncoadrian@gmail.com>
2020-06-08 22:01:38 -0400

    styled discover page

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -17,18 +17,61 @@ class Discover extends React.Component {
   render() {
     const { songs } = this.props;
     const songLinks = songs.map((song, i) => {
-    return <li key={i}><Link to={`/songs/${song.id}`}>Song {song.id}</Link></li>
-    })
+      return (
+        <li key={i}>
+          <Link to={`/songs/${song.id}`}>
+            Song {song.id}
+          </Link>
+        </li>
+      )
+    });
+
     return (
       <>
         <NavbarContainer 
           url={this.props.history.location.pathname}
         />
         <div className="discover">
-          <ul>
-            {songLinks}
-          </ul>
+          <div className="discover-main">
+            <div className="discover-content">
+              <h1>VaporWave Weekly</h1>
+              <p>All of VaporWave. Just for you.</p>
+              <div className="discover-weekly">
+                <div className="weekly-photo">
+
+                </div>
+                <div className="weekly-song-list">
+                  <ul>
+                    {songLinks}
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="discover-content">
+              <h1>New Music Now</h1>
+              <p>The latest hits, updated all the time</p>
+              <div className="discover-new">
+
+              </div>
+            </div>
+
+            <div className="discover-content">
+              <h1>Stay Home</h1>
+              <p>Tunes for isolation and self-care</p>
+              <div className="discover-new">
+
+              </div>
+            </div>
+          </div>
+
+          <div className="discover-sidebar">
+            <div className="discover-sidebar-main">
+
+            </div>
+          </div>
         </div>
+
         <MusicPlayerContainer />
       </>
 
