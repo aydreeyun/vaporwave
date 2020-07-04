aaa323452c912ef597a0490dafc7d292b00f21fe
aydreeyun <uncoadrian@gmail.com>
2020-07-04 00:19:33 -0400

    finished comments redux cycle

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,42 @@
+import * as CommentAPIUtil from '../util/comment_api_util';
+
+export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+
+const receiveComments = comments => {
+  return {
+    type: RECEIVE_COMMENTS,
+    comments
+  };
+};
+
+const receiveComment = comment => {
+  return {
+    type: RECEIVE_COMMENT,
+    comment
+  };
+};
+
+const removeComment = commentId => {
+  return {
+    type: REMOVE_COMMENT,
+    commentId
+  };
+};
+
+export const fetchComments = () => dispatch => {
+  return CommentAPIUtil.fetchComments()
+    .then(comments => dispatch(receiveComments(comments)));
+};
+
+export const createComment = comment => dispatch => {
+  return CommentAPIUtil.createComment(comment)
+    .then(comment => dispatch(receiveComment(comment)));
+};
+
+export const deleteComment = commentId => dispatch => {
+  return CommentAPIUtil.deleteComment(commentId)
+    .then(() => dispatch(removeComment(commentId)));
+};
+
